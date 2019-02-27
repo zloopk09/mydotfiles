4766b23e3b8c7ac1f334bb36f69eae1b491f72e1
zloopk09 <zloopk09@gmail.com>
2019-02-27 09:52:28 +0800

    增加aosp的下载

---
+++ AOSP/README.md
@@ -0,0 +1,2 @@
+AOSP镜像下载方法
+https://mirrors.tuna.tsinghua.edu.cn/help/AOSP/
---
+++ AOSP/aosp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -c https://mirrors.tuna.tsinghua.edu.cn/aosp-monthly/aosp-latest.tar # 下载初始化包
+tar xf aosp-latest.tar
+cd AOSP   # 解压得到的 AOSP 工程目录
+# 这时 ls 的话什么也看不到，因为只有一个隐藏的 .repo 目录
+repo sync # 正常同步一遍即可得到完整目录
+# 或 repo sync -l 仅checkout代码
